b8bff8e7d67ca0423e9a6c16b8c814d468c95367
Saqlain Mushtaq <108394350+itssaqlain06@users.noreply.github.com>
2024-12-25 02:47:39 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/My-Portfolio/',
+  base: '/',
+  server: {
+    host: true,
+    port: 5173,
+  },
   plugins: [react()],
 })
